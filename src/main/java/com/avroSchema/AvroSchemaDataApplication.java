b5885b78568ee9af10c572c287e3ad835114c068
younglae <kimyl7861@gmail.com>
2022-04-13 11:32:03 +0900

    DataSpec 완료 2

--- src/main/java/com/avroSchema/AvroSchemaDataApplication.java
+++ src/main/java/com/avroSchema/AvroSchemaDataApplication.java
@@ -2,7 +2,7 @@ package com.avroSchema;
 
 import com.avroSchema.data.Data;
 import com.avroSchema.data.DataSpec;
-import com.avroSchema.service.Process;
+import com.avroSchema.data.SheetAt;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -11,10 +11,11 @@ public class AvroSchemaDataApplication {
 	public static final String filePath = "C:/Users/220209/Desktop/서산_테이블정의서_20220315.xlsx";
 
 	public static void main(String[] args) {
-		Process pcs = new Process(filePath);
+		SheetAt pcs = new SheetAt(filePath);
 		DataSpec dts = new DataSpec(pcs.sheet);
-		pcs.sheet = pcs.getSheetAt(5);
 		Data dt = new Data(pcs.sheet);
+
+
 
 //		SpringApplication.run(AvroSchemaDataApplication.class, args);
 	}
--- src/main/java/com/avroSchema/data/SheetAt.java
+++ src/main/java/com/avroSchema/data/SheetAt.java
@@ -1,6 +1,5 @@
-package com.avroSchema.service;
+package com.avroSchema.data;
 
-import com.avroSchema.data.Data;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -8,23 +7,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Process {
+public class SheetAt {
 
     public XSSFWorkbook workbook;
     public XSSFSheet sheet;
-    public final String filePath = "C:/Users/220209/Desktop/서산_테이블정의서_20220315.xlsx";
 
-    public Process(String filePath){
-
+    public SheetAt(String filePath){
         try{
             FileInputStream fileInputStream = new FileInputStream(filePath);
             workbook = new XSSFWorkbook(fileInputStream);
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
+        }catch (FileNotFoundException e){e.printStackTrace();
+        }catch (IOException e){e.printStackTrace();}
     }
 
     public XSSFSheet getSheetAt(int index){
