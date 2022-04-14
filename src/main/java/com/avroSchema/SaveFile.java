2ef6d408c9e90c01af635636580f2ab94c645d23
younglae <kimyl7861@gmail.com>
2022-04-14 16:20:14 +0900

    구조 다시 잡기 - 완성 2.

--- src/main/java/com/avroSchema/SaveFile.java
+++ src/main/java/com/avroSchema/SaveFile.java
@@ -11,19 +11,17 @@ import java.util.Arrays;
 
 public class SaveFile {
 
-    File dir;
+    File dir = new File("../");
     String[] filenames;
     boolean isDirExist;
-    Path pathBefore;
+    Path pathBefore = Paths.get("../txtAvroSchemaFiles/");
 
     // 파일 저장하기
     public void SaveFile(XSSFSheet sheet) throws IOException{
         DataSpec dataSpec = new DataSpec(sheet);
 
-        dir = new File("../");
         filenames = dir.list();
         isDirExist = Arrays.asList(filenames).contains("txtAvroSchemaFiles");
-        pathBefore = Paths.get("../txtAvroSchemaFiles/");
 
         // 해당 경로의 폴더가 존재하지 않으면 폴더 만들어서 저장하기기
         if(!isDirExist){Files.createDirectory(pathBefore);}
